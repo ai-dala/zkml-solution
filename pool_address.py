30ae77648fb1d394fbce38e3637f2f6738648ac1
royalnine <17271567+royalnine@users.noreply.github.com>
2024-05-29 17:52:37 +0500

    format

--- pool_address.py
+++ pool_address.py
@@ -6,18 +6,16 @@ factory_abi = [
         "inputs": [
             {"internalType": "address", "name": "tokenA", "type": "address"},
             {"internalType": "address", "name": "tokenB", "type": "address"},
-            {"internalType": "uint24", "name": "fee", "type": "uint24"}
+            {"internalType": "uint24", "name": "fee", "type": "uint24"},
         ],
         "name": "getPool",
-        "outputs": [
-            {"internalType": "address", "name": "pool", "type": "address"}
-        ],
+        "outputs": [{"internalType": "address", "name": "pool", "type": "address"}],
         "stateMutability": "view",
-        "type": "function"
+        "type": "function",
     }
 ]
 
-infura_url = 'https://sepolia.infura.io/v3/8ef2abc2fcac40fdad51d7891ae4bee5'
+infura_url = "https://sepolia.infura.io/v3/8ef2abc2fcac40fdad51d7891ae4bee5"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 if web3.is_connected():
@@ -25,8 +23,11 @@ if web3.is_connected():
 else:
     print("Failed to connect")
 
-factory_address = '0x0227628f3F023bb0B980b67D528571c95c6DaC1c'  # Uniswap V3 factory address
+factory_address = (
+    "0x0227628f3F023bb0B980b67D528571c95c6DaC1c"  # Uniswap V3 factory address
+)
 factory_contract = web3.eth.contract(address=factory_address, abi=factory_abi)
+
 
 def get_uniswap_v3_pool_address(tokenA, tokenB, fee):
     try:
@@ -37,10 +38,11 @@ def get_uniswap_v3_pool_address(tokenA, tokenB, fee):
     except Exception as e:
         return f"Error fetching pool address: {str(e)}"
 
+
 # Example usage:
-tokenA = '0x5928A372De475721231B4411a26a01602E0a6dFa'  # DAI
-tokenB = '0xdCdC73413c6136c9abcc3e8d250af42947ac2fc7'  # USDC
+tokenA = "0x5928A372De475721231B4411a26a01602E0a6dFa"  # DAI
+tokenB = "0xdCdC73413c6136c9abcc3e8d250af42947ac2fc7"  # USDC
 fee = 3000  # Fee tier (e.g., 3000 for 0.3%)
 
 pool_address = get_uniswap_v3_pool_address(tokenA, tokenB, fee)
-print(f'The pool address is: {pool_address}')
+print(f"The pool address is: {pool_address}")
--- src/data_loader.py
+++ src/data_loader.py
@@ -5,30 +5,30 @@ import os
 
 def load_electricity() -> DataFrame:
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, 'data', 'electricity_costs.csv')
+    file_path = os.path.join(current_dir, "data", "electricity_costs.csv")
 
     return DataFrame(read_csv(file_path))
 
 
 def load_btc_prices() -> DataFrame:
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('asics-dynamodb-PricesTable-1LXU143BUOBN')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("asics-dynamodb-PricesTable-1LXU143BUOBN")
     response = table.scan()
-    items = response['Items']
+    items = response["Items"]
     df = DataFrame(items)
     df["Date"] = to_datetime(df["Date"])
-    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-    df.sort_values(by='Date', inplace=True)
+    df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+    df.sort_values(by="Date", inplace=True)
     return df
 
 
 def load_difficulties() -> DataFrame:
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('asics-dynamodb-DifficultyTable-DQ308ID3POT6')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("asics-dynamodb-DifficultyTable-DQ308ID3POT6")
     response = table.scan()
-    items = response['Items']
+    items = response["Items"]
     df = DataFrame(items)
-    df.sort_values(by='Date', inplace=True)
+    df.sort_values(by="Date", inplace=True)
     return df
 
 
@@ -37,7 +37,7 @@ btc_prices = load_btc_prices()
 difficulties = load_difficulties()
 
 # Join dataframes by Date
-merged_df = electricity.merge(btc_prices, on='Date')
-merged_df = merged_df.merge(difficulties, on='Date')
+merged_df = electricity.merge(btc_prices, on="Date")
+merged_df = merged_df.merge(difficulties, on="Date")
 
 print(0)
